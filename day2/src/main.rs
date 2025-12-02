aa99293092a23fe381c2c0ced9d99a608413f3c5
Domenic Marcelli <spartanxd31@vt.edu>
2025-12-02 16:19:56 -0500

    Day2 part1 done

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,4 @@
+use core::num;
 use std::env;
 use std::fs;
 use std::vec;
@@ -8,11 +9,24 @@ fn main() {
     // to get rid of the new line which is for somewhere there.
     contents.pop();
     let ranges: Vec<&str> = contents.split(",").collect();
-    println!("{:?}", ranges);
+    let mut sum: u64 = 0;
+
     for range in ranges {
         let bounds: Vec<&str> = range.split("-").collect();
         let lower: u64 = bounds[0].parse().expect("Not a valid number");
         let upper: u64 = bounds[1].parse().expect("Not a valid number");
-        println!("{lower}, {upper}");
+        for i in lower..(upper + 1) {
+            let num_string = i.to_string();
+            // split in half
+            let num_len = num_string.len();
+            if num_len.rem_euclid(2) == 0 {
+                let (pair_one, pair_two) = num_string.split_at(num_len / 2);
+                if pair_one == pair_two {
+                    // sum += num_string.parse::<u64>().expect("Should be an u64");
+                    sum += i;
+                }
+            }
+        }
     }
+    println!("{sum}");
 }
