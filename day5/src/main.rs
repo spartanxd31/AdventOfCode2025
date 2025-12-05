8481c8049afb803f1a22a7450b8e1272e0bac59a
Domenic Marcelli <spartanxd31@vt.edu>
2025-12-05 15:14:26 -0500

    part2 complete

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -2,10 +2,6 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-fn check_intersection(low1: u64, low2: u64, upper1: u64, upper2: u64) -> bool {
-    low1 < upper2 && low2 < upper1
-}
-
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
@@ -50,4 +46,43 @@ fn main() {
         }
     }
     println!("{fresh}");
+    //part2
+    let mut parsed_ranges: Vec<(u64, u64)> = Vec::new();
+    for range in ranges {
+        let bounds: Vec<&str> = range.split("-").collect();
+        let lower: u64 = bounds[0]
+            .to_string()
+            .parse()
+            .expect("This should be a range");
+        let upper: u64 = bounds[1]
+            .to_string()
+            .parse()
+            .expect("This should be a range");
+        parsed_ranges.push((lower, upper));
+    }
+
+    // Sort ranges by lower bound
+    parsed_ranges.sort_by_key(|&(low, _)| low);
+
+    let mut merged: Vec<(u64, u64)> = Vec::new();
+    for (lower, upper) in parsed_ranges {
+        if merged.is_empty() {
+            merged.push((lower, upper));
+        } else {
+            let last_idx = merged.len() - 1;
+            let (last_lower, last_upper) = merged[last_idx];
+
+            if lower <= last_upper + 1 {
+                merged[last_idx] = (last_lower, std::cmp::max(last_upper, upper));
+            } else {
+                merged.push((lower, upper));
+            }
+        }
+    }
+
+    let mut total = 0;
+    for (lower, upper) in merged {
+        total += upper - lower + 1;
+    }
+    println!("Part 2: {total}");
 }
