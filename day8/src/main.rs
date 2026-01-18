04ffba611109e8c404f97af936ec5c03abc82a9d
Domenic Marcelli <spartanxd31@vt.edu>
2026-01-17 22:01:52 -0500

    Working on day 8 solution. Added flake for working on my nixos machine

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -9,18 +9,60 @@ where
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
-
+#[derive(Copy, Clone)]
 struct Coords {
-    x: u64,
-    y: u64,
-    z: u64,
+    x: f64,
+    y: f64,
+    z: f64,
 }
 
-struct Electrical {
-    loc: Vec<Coords>,
-    dis: u64,
+impl Coords {
+    fn from_str(s: &str) -> Option<Self> {
+        let parts: Vec<f64> = s.split(',').filter_map(|p| p.trim().parse().ok()).collect();
+
+        if parts.len() == 3 {
+            Some(Coords {
+                x: parts[0],
+                y: parts[1],
+                z: parts[2],
+            })
+        } else {
+            None
+        }
+    }
+}
+
+struct WirePairs {
+    item1: Coords,
+    item2: Coords,
+    dis: f64,
+}
+
+fn distance(a: Coords, b: Coords) -> f64 {
+    let dx = (a.x - b.x).powf(2.0);
+    let dy = (a.y - b.y).powf(2.0);
+    let dz = (a.z - b.z).powf(2.0);
+    (dx + dy + dz).sqrt()
 }
 
 fn main() {
-    println!("Hello, world!");
+    if let Ok(lines) = read_lines("./test.txt") {
+        let coords_list: Vec<_> = lines
+            .filter_map(|line| Coords::from_str(&line.ok()?))
+            .collect();
+        let junction_box: Vec<Vec<Coords>> = Vec::new();
+        let mut pairs: Vec<WirePairs> = Vec::new();
+        for ca in &coords_list {
+            for cb in coords_list.clone() {
+                let some_pair = WirePairs {
+                    item1: *ca,
+                    item2: cb,
+                    dis: distance(*ca, cb),
+                };
+                pairs.push(some_pair);
+            }
+        }
+        println!("{}", pairs.len());
+        pairs.sort_by(|a, b| a.dis.partial_cmp(&b.dis).expect("Something"));
+    }
 }
