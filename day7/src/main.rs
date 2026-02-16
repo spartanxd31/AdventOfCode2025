122b29cea54390d6b80279853ad7c4d743b7cd72
Domenic Marcelli <spartanxd31@vt.edu>
2026-02-16 18:54:32 -0500

    finished day 7 part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -46,10 +46,8 @@ fn part1(filename: &str) -> u32 {
     }
     return count;
 }
-fn part2(filename: &str) -> u32 {
-    let mut count = 0;
-    let mut paths = 0;
-    //I don't know if I need this but it might be useful for keeping track
+fn part2(filename: &str) -> u64 {
+    let mut paths: Vec<u64> = vec![0; 1000];
     if let Ok(lines) = read_lines(filename) {
         let mut prev_line: Vec<char> = Vec::new();
         for (index, line) in lines.enumerate() {
@@ -62,33 +60,38 @@ fn part2(filename: &str) -> u32 {
                 for (i, ch) in current_line.iter().enumerate() {
                     if prev_line[i] == 'S' {
                         modified[i] = '|';
+                        paths[i] += 1;
                     } else if prev_line[i] == '|' && modified[i] != '^' {
                         modified[i] = '|';
-                    } else if modified[i] == '^' && prev_line[i] == '|' {
-                        if modified[i - 1] == '.' {
-                            modified[i - 1] = '|';
+                    } else if modified[i] == '^' {
+                        paths[i - 1] += paths[i];
+                        if (i + 1) < modified.len() {
                             //paths += 1;
+                            paths[i + 1] += paths[i];
                         }
-                        if (i + 1) < modified.len() && modified[i + 1] == '.' {
-                            modified[i + 1] = '|';
-                            //paths += 1;
-                        }
-                        count += 1;
-                        paths += 2;
+
+                        paths[i] = 0;
                     }
                 }
-
+                print!(" ");
+                for (i, p) in paths.clone().iter().enumerate() {
+                    if i < 15 {
+                        print!("{}", p);
+                    }
+                }
+                println!("");
                 prev_line = modified.clone();
             }
+
             let line_string: String = prev_line.clone().into_iter().collect();
             println!("{:?}", line_string)
         }
     }
-    return paths;
+    return paths.iter().sum();
 }
 fn main() {
-    let answer = part1("./input.txt");
-    println!("{}", answer);
-    let answer2 = part2("./test.txt");
+    // let answer = part1("./input.txt");
+    // println!("{}", answer);
+    let answer2 = part2("./input.txt");
     println!("{}", answer2);
 }
